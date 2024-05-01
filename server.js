e54c3b657035934fbc9772b2bb7ed914b8dded0a
RyanCPotts <ryancpotts@gmail.com>
2024-04-30 19:51:34 -0500

    basic express app setup

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+//Route Definitions
+app.get('/', getHomePage);
+app.get('/data', timeStamper, getData);
+
+function getHomePage(req, res) {
+  res.status(200).send('Hello World');
+}
+
+function timeStamper(req, res, next) {
+  // res.status(200).send('Sushi Pants');
+  req.timeStamp=new Date();
+  next()
+}
+
+function getData(req, res) {
+  res.status(200).send(req.timeStamp)
+}
+
+
+
+
+
+function start(port) {
+  app.listen(port, () => {
+    console.log(`Server up on port ${port}`)
+
+  });
+}
+
+module.exports = {
+  app: app,
+  start: start,
+};
+
